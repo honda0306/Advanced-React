6b3c3134be74959dbcc1ce3f6811ac443d01aa80
Chris Honda <christopher.honda92@gmail.com>
2018-11-24 23:04:14 -0800

    Created index.js and sell.js; added Link for routing

---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Sell = props => (
+    <div>
+        <p>Sellable thingathings!</p>
+        <Link href="/">
+            <a>Home!</a>
+        </Link>
+    </div>
+);
+
+export default Sell;
