3a3b1e23744fc02c5f45bf094a82b27120116a0c
Chris Honda <christopher.honda92@gmail.com>
2018-11-26 16:59:32 -0800

    Custom _app.js Layout, including Header, Meta, Nav, and Page

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -3,9 +3,6 @@ import Link from 'next/link';
 const Home = props => (
     <div>
         <p>Hello there!</p>
-        <Link href="/sell">
-            <a>Sell!</a>
-        </Link>
     </div>
 )
 
--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -3,9 +3,6 @@ import Link from 'next/link';
 const Sell = props => (
     <div>
         <p>Sellable thingathings!</p>
-        <Link href="/">
-            <a>Home!</a>
-        </Link>
     </div>
 );
 
